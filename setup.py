634541209bd3d81cdef7a1ce1c972f2faf988133
Ajay MT <ajay.tatachar@gmail.com>
2015-05-13 11:45:53 +0530

    v0.0.7

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name='emitter',
-    version='0.0.6',
+    version='0.0.7',
     description='simple event emitter',
     author='Ajay MT',
     author_email='ajaymt@icloud.com',
     url='http://github.com/ajaymt/emitter',
-    download_url='https://github.com/AjayMT/emitter/tarball/v0.0.6',
+    download_url='https://github.com/AjayMT/emitter/tarball/v0.0.7',
     py_modules=['emitter'],
     keywords='emitter event eventemitter node',
     classifiers=[
